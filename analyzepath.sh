0d2360a2907e92648000e5e532a0dea78f4f6df7
Stepan Rutz <stepan.rutz@gmx.de>
2024-02-15 10:14:17 +0100

    ore

--- analyzepath.sh
+++ analyzepath.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+P=/usr/bin:/bin:/usr/sbin
 
-IFS=":"; for ENTRY in $PATH; do
-	NUMENTRIES=$(test -d $ENTRY && ls -l "$ENTRY" | wc -l)
+IFS=":"; for ENTRY in $P; do
+	NUMENTRIES=$(test -d "$ENTRY" && ls -l "$ENTRY" | wc -l)
 	
 	printf "%5g entries in: %s\n" ${NUMENTRIES:-"0"} "$ENTRY"
 done | sort -rn
